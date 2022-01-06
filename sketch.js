176d7d5a0ab9d5c6bab46c215c191a554965748b
kritikhatter <83900188+kritikhatter@users.noreply.github.com>
2022-01-06 20:10:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,19 +3,31 @@ var bg, backgroundImg;
 
 function preload() {
   backgroundImg = loadImage("images/bg.jpg");
- 
+ ironmanImage=loadAnimation("images/iron.png");
 }
 
 function setup() {
   createCanvas(1000, 600);
   bg = createSprite(580,300);
- 
+  bg.addImage(backgroundImg);
+  bg.scale=1.5
+  ironman=createSprite(200,505,20,50)
+  ironman.addAnimation("jumping",ironmanImage)
+  ironman.scale=0.3
+  ground=createSprite(200,585,400,10)
+  ground.visible=false
 }
 
+
 function draw() {
-  
- 
-    
+  if(keyDown("left"))
+  ironman.x-=4
+  if (keyDown("right"))
+  ironman.x+=4
+  if(keyDown("space"))
+    ironman.velocityY=-16
+    ironman.velocityY+=0.5
+ironman.collide(ground)
     drawSprites();
    
 }
